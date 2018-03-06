7fb89a7549f538a33a1828fb10f857f877954a7f
dan <elastic7327@gmail.com>
2018-03-06 23:27:41 +0900

    added

--- tests/base.py
+++ tests/base.py
@@ -6,8 +6,13 @@ class BaseTest(tf.test.TestCase):
 
     def setUp(self):
         print("Let's Start with TensorFlow!")
+
+        # *******************************************************************************************
         import os
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+        # Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX AVX2
+        # issue solution
+        # *******************************************************************************************
 
     def tearDown(self):
         print("Bye TensorFlow")
--- tests/chapter_2/test_bandit.py
+++ tests/chapter_2/test_bandit.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from tests.base import BaseTest
 
 class TestSmokeTest(BaseTest):
