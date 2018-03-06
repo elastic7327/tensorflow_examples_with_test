b5aabc50971d5b45e0c7d230d0e353bc407b48a1
dan <elastic7327@gmail.com>
2018-03-06 21:50:41 +0900

    SetUp Agent Class

--- tests/chapter_3/test_context_bandit.py
+++ tests/chapter_3/test_context_bandit.py
@@ -41,10 +41,46 @@ class ContextualBandit(object):
             return -1
 
 
+class Agent():
+
+    __slots__ = ["state_in", "state_in_OH", "output", "chosen_action", "reward_holder", "action_holder", "responsible_weight", "loss", "optimizer", "update"]
+
+    def __init__(self, lr, s_size, a_size):
+        # 네트워크의 피드포워드 부분, 에이전트는 상태를 받아서 액션을 출력한다.
+        self.state_in = tf.placeholder(shape=[1], dtype=tf.int32)
+        state_in_OH = slim.one_hot_encoding(self.state_in, s_size)
+        output = slim.fully_connected(state_in_OH, a_size, biases_initializer=None, activation_fn=tf.nn.sigmoid, weights_initializer=tf.ones_initializer())
+
+        self.output = tf.reshape(output, [-1])
+        self.chosen_action = tf.argmax(self.output, 0)
+
+        # 학습과정을 구현한다.
+        # 비용을 계산하기 위해 보상과 선택된 액션을 네트워크에 피드하고,
+        # 네트워크를 업데이트하는 데에 이를 이용한다.
+
+        self.reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
+
+        self.responsible_weight = tf.slice(self.output, self.action_holder, [1])
+        self.loss = -(tf.log(self.responsible_weight)* self.reward_holder)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+        self.update = optimizer.minimize(self.loss)
+
+
+
 class TestContextBanditTest(BaseTest):
 
     def test_class_init_test(self):
         res = ContextualBandit()
 
     def test_context_bandit(self):
-        pass
+
+        # 텐서플로 그래프를 리셋한다.
+        tf.reset_default_graph()
+
+        # 벤딧을 로드한다.
+        cBandit = ContextualBandit()
+
+        # 에이전트를 로드한다.
+
+        myAgent = Agent(lr=0.001, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
